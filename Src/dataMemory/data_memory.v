598be34d918e45ef4cf8523a4272ea398c695146
Mari Fredriksen <fredriksenmari@gmail.com>
2018-11-28 09:59:41 +0100

    Control file + data_memory

---
+++ Src/dataMemory/data_memory.v
@@ -0,0 +1,34 @@
+module  data_memory(address, instruction, write_data, memRead, memWrite);
+
+input [31:0]address;
+input [31:0]write_data;
+input memWrite; //Conrol bits: =1 -> The current adr is passed to read data 
+input memRead; //Control bits 
+output [31:0]read_data; //Data read from memory to be written in register 
+
+wire address;
+wire write_data;
+wire memWrite;
+wire memRead; 
+reg read_data;
+reg [7:0] memory [0:255];
+
+
+initial begin
+ $readmemb("../Resources/data_memory.list", memory);
+end 
+
+always @(address or memRead or memWrite or write_data) begin 
+	if(memWrite == 1'b1) begin
+		memory[address] = write_data;
+
+	end 
+	
+	else if (memRead == 1'b1) begin
+	//Not sure what will happen in here..
+		read_data = memory[address];
+	end
+
+
+end
+endmodule
--- Src/decode/control.v
+++ Src/decode/control.v
@@ -5,7 +5,7 @@ output [12:0]controlBits;
 
 wire opcode;
 reg controlBits;
-reg regDst, branch, memRead, memToReg, memWrite, aluSrc, regWrite;
+reg regDst, branch, memRead, memToReg, memWrite, aluSrc, regWrite, jump;
 always @ (opcode) begin
 case(opcode) //Update with the reals values
   6'b0 : begin // Opcode 0x0 - ADD
@@ -16,21 +16,104 @@ case(opcode) //Update with the reals values
       memWrite = 1'b0; 
       aluSrc = 1'b0;
       regWrite = 1'b1;
+      jump = 1'b0;
             end
-  6'b111111 : begin
+  6'b000001 : begin // Opcode 0x1 - SUB
       regDst = 1'b1; 
-      branch = 1'b1;
+      branch = 1'b0;
+      memRead = 1'b0;
+      memToReg = 1'b0;
+      memWrite = 1'b0; 
+      aluSrc = 1'b0;
+      regWrite = 1'b1;
+      jump = 1'b0;
+            end
+  6'b000010 : begin // Opcode 0x2 - MUL
+      regDst = 1'b1; 
+      branch = 1'b0;
+      memRead = 1'b0;
+      memToReg = 1'b0;
+      memWrite = 1'b0; 
+      aluSrc = 1'b0;
+      regWrite = 1'b1;
+      jump = 1'b0;
+            end
+  6'b001010 : begin // Opcode 0x10 - LDB: //To sign Extend, to 32 bits in register 
+      regDst = 1'b1; 
+      branch = 1'b0;
       memRead = 1'b1;
       memToReg = 1'b1;
-      memWrite = 1'b1; 
+      memWrite = 1'b0; 
       aluSrc = 1'b1;
       regWrite = 1'b1;
+      jump = 1'b0;
     end
+
+    //Optional?
+  6'b001011 : begin // Opcode 0x11 - LDW
+      regDst = 1'b0; 
+      branch = 1'b0;
+      memRead = 1'b1;
+      memToReg = 1'b1;
+      memWrite = 1'b0; 
+      aluSrc = 1'b1;
+      regWrite = 1'b1;
+      jump = 1'b0;
+            end
+  6'b001100 : begin // Opcode 0x12 - STB
+      regDst = 1'b0; 
+      branch = 1'b0;
+      memRead = 1'b0;
+      memToReg = 1'b0;
+      memWrite = 1'b1; 
+      aluSrc = 1'b1;
+      regWrite = 1'b0;
+      jump = 1'b0;
+    end
+
+    //Optional?
+
+  6'b001101 : begin // Opcode 0x13 - STW
+      regDst = 1'b0; 
+      branch = 1'b0;
+      memRead = 1'b0;
+      memToReg = 1'b0;
+      memWrite = 1'b1; 
+      aluSrc = 1'b1;
+      regWrite = 1'b0;
+      jump = 1'b0;
+    end
+
+
+//PC 
+  6'b011110 : begin // Opcode 0x30 - BEQ
+      regDst = 1'b0; 
+      branch = 1'b1;
+      memRead = 1'b0;
+      memToReg = 1'b0;
+      memWrite = 1'b0; 
+      aluSrc = 1'b0;
+      regWrite = 1'b0;
+      jump = 1'b0;
+    end
+
+  6'b011111 : begin // Opcode 0x31 - JUMP 
+      regDst = 1'b1; 
+      branch = 1'b1;
+      memRead = 1'b0;
+      memToReg = 1'b0;
+      memWrite = 1'b0; 
+      aluSrc = 1'b0;
+      regWrite = 1'b0;
+      jump = 1'b1;
+    end
+
+
   default : begin
               $display("@%0dns default is selected, opcode %b",$time,opcode);
             end
 endcase
-controlBits = {regDst, branch, memRead, memToReg, memWrite, aluSrc, regWrite, opcode};
+controlBits = {regDst, branch, memRead, memToReg, memWrite, aluSrc, regWrite, opcode, jump};
 end
 
 endmodule
