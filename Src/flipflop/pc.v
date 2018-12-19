944adf6e1f162530fce63938da097e38ce821180
Louise Fonteneau <louise.fonteneau@insa-lyon.fr>
2018-12-19 15:11:42 +0100

    add mux to PC module

--- Src/flipflop/pc.v
+++ Src/flipflop/pc.v
@@ -4,12 +4,15 @@ input clock, rst;
 
 wire we;
 reg [31:0]pc;
+wire [31:0]newPc;
+
+mux32 mux32(.in1(fetch.pcIncr), .in2(ex_mem.pcBranch), .ctrl(ex_mem.pcSrc), .out(newPc));
 
 always @ (posedge clock) begin
 	if (rst) begin
 		pc <= 0;
 	end else if (we) begin 
-		pc <= firstCPU.newPc;
+		pc <= newPc;
 	end
 end
 
