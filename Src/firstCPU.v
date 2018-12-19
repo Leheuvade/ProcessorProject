c0c24f30e15c39541b01ca69758c1899d4164e1f
Louise Fonteneau <louise.fonteneau@insa-lyon.fr>
2018-12-19 12:04:13 +0100

    clean main

--- Src/firstCPU.v
+++ Src/firstCPU.v
@@ -16,65 +16,9 @@
 module firstCPU;
 
 reg clock;
-
-//Variables shared between stages 
-wire flushPrevInstr;
-
-//Control Bits pc FF
-wire write_PC;
 reg rst_PC;
-
-//Variables used in Fetch stage
-wire [31:0]newPc;
-
-//Control Bits IFID FF
 reg rst_IFID;
-wire write_IFID;
-
-//Output IFID FF
-wire [31:0]pc_IFID; 
-wire [31:0]instruction_IFID;
-
-//Variables used in Decode stage
-wire [31:0]readData1, readData2, address; 
-wire [4:0]rdRegister, rsRegister, rtRegister;
-wire [1:0]aluCtrl;
-wire [0:8]controlBits;
-wire zero;
-
-//Control Bits IDEX FF
-
-//Output IDEX FF
-wire [0:8]controlBits_IDEX;
-wire branch_IDEX, aluSrc_IDEX;
-wire [1:0]aluCtrl_IDEX;
-wire memRead_IDEX;
-wire [31:0]readData1_IDEX, readData2_IDEX, address_IDEX, pc_IDEX;
-wire [4:0]rd_IDEX, rs_IDEX, rt_IDEX;
-
-
-//Variables used in Exec stage
-wire [31:0]result, pcBranch;
-wire pcSrc;
-
-//Control Bits EXMEM FF
-
-//Output EXMEM FF
-wire [0:8]controlBits_EXMEM;
-wire memRead_EXMEM, memWrite_EXMEM, word_EXMEM, regWrite_EXMEM, pcSrc_EXMEM, flushPrevInstr_EXMEM;
-wire [31:0]readData2_EXMEM, result_EXMEM, pcBranch_EXMEM;
-wire [4:0]rd_EXMEM; 
-
-//Variables used in Mem stage
-wire [31:0]readDataM;
-
-//Output MEMWB FF
-wire memToReg_MEMWB, regWrite_MEMWB;
-wire [31:0]result_MEMWB, readDataM_MEMWB;
-wire [4:0]rd_MEMWB;
- 
-//Variables used in WB stage
-wire [31:0]valueToWB;
+wire [31:0]newPc;
 
 initial begin
   $dumpfile("firstCPU.vcd");
@@ -95,47 +39,33 @@ end
 mux32 mux32(.in1(fetch.pcIncr), .in2(ex_mem.pcBranch), .ctrl(ex_mem.pcSrc), .out(newPc));
 
 //Flip Flop PC 
-pc pc(
-	.rst(rst_PC), 
-	.clock(clock)
-);
+pc pc(.rst(rst_PC), .clock(clock));
 
 //Fetch stage 
 fetch fetch();
 
 //Flip Flop IF_ID
-if_id if_id( 
-	.rst(rst_IFID),
-	.flush(ex_mem.flushPrevInstr),
-	.clock(clock)
-);
+if_id if_id(.rst(rst_IFID), .flush(ex_mem.flushPrevInstr), .clock(clock));
 
 //Decode stage 
 decode decode();
 
 //Flip Flop ID_EX
-id_ex id_ex(
-	.flush(ex_mem.flushPrevInstr),
-	.clock(clock)
-);
+id_ex id_ex(.flush(ex_mem.flushPrevInstr), .clock(clock));
 
 //Exec stage
 exec exec();
 
 //Flip Flop EX_MEM
-ex_mem ex_mem( 
-	.flush(ex_mem.flushPrevInstr),
-	.clock(clock)
-);
+ex_mem ex_mem(.flush(ex_mem.flushPrevInstr), .clock(clock));
 
 //Memory stage 
 memory memory();
 
 //Flip Flop MEM_WB
-mem_wb mem_wb(
-	.clock(clock)
-);
+mem_wb mem_wb(.clock(clock));
 
 //Write Back stage
 wb wb();
+
 endmodule
