8fea345e88efd5bd8f07f904f87d30ee32f6bf64
jeanpaultarot@gmail.com <jeanpaultarot@gmail.com>
2019-01-06 06:32:05 +0100

    First version of cache

---
+++ Src/cache/preprocessor_directives.v
@@ -0,0 +1,11 @@
+'ifndef _cache_preprocessor_directives
+'define _cache_preprocessor_directives
+'include "../../preprocessor_directives.v"
+
+'define CACHE_LINES 4
+'define LINE_ADDR_SIZE ($clog2('CACHE_LINES))
+'define LINE_ADDR_START_INDEX ($clog2('LINE_WIDTH) - 3)
+'define CACHE_ADDR_RANGE ('LINE_ADDR_START_INDEX+'LINE_ADDR_SIZE-1:'LINE_ADDR_START_INDEX)
+'define TAG_SIZE ('PHYS_ADDR_SIZE - 'LINE_ADDR_SIZE - 'LINE_ADDR_START_INDEX)
+  
+'endif
