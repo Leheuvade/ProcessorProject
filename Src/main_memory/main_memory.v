6c52f99bbdc8147ad8c97c76a561ae4770e105f0
Leheuvade <29398330+Leheuvade@users.noreply.github.com>
2019-01-16 13:45:23 +0100

    update main_memory

--- Src/main_memory/main_memory.v
+++ Src/main_memory/main_memory.v
@@ -1,5 +1,3 @@
-`include "preprocessor_directives.v"
-
 module main_memory();
 
 localparam MEM_LINES_INDEX_SIZE = $clog2(`MEM_LINES);
@@ -7,9 +5,20 @@ localparam MEM_LINES_INDEX_SIZE = $clog2(`MEM_LINES);
 reg [`LINE_WIDTH - 1:0] memoryFile [0:`MEM_LINES - 1];
 reg [`LINE_WIDTH - 1:0]line; 
 reg [MEM_LINES_INDEX_SIZE - 1:0]indexLine; 
+reg fillCache;
 
-always @(indexLine) begin
+initial begin
+	$readmemb("../Resources/memory.list", memoryFile);
+end 
+
+always @(arb.reqI) begin
+    $monitor ("%g\t memoryCall", $time); 
 	#`LATENCY line = memoryFile[indexLine];
+	fetch.waitInst = 0;
+	fillCache = 1;
+	arb.reqI = 0;
+	pc.we = 1;
+	if_id.clear = 0;
 end
 
 endmodule
